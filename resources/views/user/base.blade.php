75c1473b33332c9769b6e92a45cc343f8b422535
Anon Dev <michaelerastus9@gmail.com>
2025-04-15 08:48:26 +0100

    Updated

--- resources/views/user/base.blade.php
+++ resources/views/user/base.blade.php
@@ -167,53 +167,6 @@
 </script>
 @stack('js')
 
-
-<!-- Google language start -->
-<style>
-
-    #google_translate_element {
-        z-index: 9999999;
-        position: fixed;
-        bottom: 25px;
-        left: 15px;
-    }
-
-    .goog-te-gadget {
-        font-family: Roboto, "Open Sans", sans-serif !important;
-        text-transform: uppercase;
-    }
-    .goog-te-gadget-simple
-    {
-        padding: 0px !important;
-        line-height: 1.428571429;
-        color: white;
-        vertical-align: middle;
-        background-color: black;
-        border: 1px solid #a5a5a599;
-        border-radius: 4px;
-        float: right;
-        margin-top: -4px;
-        z-index: 999999;
-    }
-    .goog-te-banner-frame.skiptranslate
-    {
-        display: none !important;
-        color: white;
-    }
-    .goog-te-gadget-icon
-    {
-        background: none !important;
-        display: none;
-        color: white;
-    }
-    .goog-te-gadget-simple .goog-te-menu-value
-    {
-        font-size: 12px;
-        color: white;
-        font-family: 'Open Sans' , sans-serif;
-    }
-</style>
-<div id="google_translate_element"></div>
 <script type="text/javascript">
     window.onload = function googleTranslateElementInit() {
         new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
